47f8748138656e13e058fb4c7e3e7dff433890ef
Mikko-Matias Juntunen <mikko@mikkojuntunen.fi>
2024-09-07 20:08:56 +0300

    Add line to init.lua.

--- init.lua
+++ init.lua
@@ -831,6 +831,7 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'rose-pine/neovim',
+    name = 'rose-pine',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
