ec70cbe921090899b5a28506663013d32351cda8
Mikko-Matias Juntunen <mikko@mikkojuntunen.fi>
2024-09-07 10:47:35 +0300

    Add avante.lua to version control

---
+++ lua/custom/plugins/avante.lua
@@ -0,0 +1,64 @@
+return {
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
+  opts = {
+    -- add any opts here
+    provider = 'ollama',
+    vendors = {
+      ---@type AvanteProvider
+      ollama = {
+        ['local'] = true,
+        endpoint = '88.193.136.171:11434/v1',
+        model = 'deepseek-coder-v2:16b-lite-instruct-q8_0',
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. '/chat/completions',
+            headers = {
+              ['Accept'] = 'application/json',
+              ['Content-Type'] = 'application/json',
+            },
+            body = {
+              model = opts.model,
+              messages = require('avante.providers').copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+        parse_response_data = function(data_stream, event_state, opts)
+          require('avante.providers').openai.parse_response(data_stream, event_state, opts)
+        end,
+      },
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = 'make',
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    --- The below dependencies are optional,
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    'zbirenbaum/copilot.lua', -- for providers='copilot'
+    {
+      -- support for image pasting
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+}
